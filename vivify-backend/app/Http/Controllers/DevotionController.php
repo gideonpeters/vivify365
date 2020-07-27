eb20d50e982d298081877e761126b0e53ea12f82
Gideon Peters <gideonpeters85@gmail.com>
2020-07-27 12:42:50 +0100

    chore: backend models and migrations init

---
+++ vivify-backend/app/Http/Controllers/DevotionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Devotion;
+use Illuminate\Http\Request;
+
+class DevotionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Devotion  $devotion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Devotion $devotion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Devotion  $devotion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Devotion $devotion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Devotion  $devotion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Devotion $devotion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Devotion  $devotion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Devotion $devotion)
+    {
+        //
+    }
+}
---
+++ vivify-backend/app/Http/Controllers/GalleryPictureController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\GalleryPicture;
+use Illuminate\Http\Request;
+
+class GalleryPictureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\GalleryPicture  $galleryPicture
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GalleryPicture $galleryPicture)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\GalleryPicture  $galleryPicture
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GalleryPicture $galleryPicture)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\GalleryPicture  $galleryPicture
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GalleryPicture $galleryPicture)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\GalleryPicture  $galleryPicture
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GalleryPicture $galleryPicture)
+    {
+        //
+    }
+}
